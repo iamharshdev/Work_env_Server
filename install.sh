316f94ebdf251418958a8877ec74268964f3fa06
iamharshdev <nitingoswami473@gmail.com>
2020-11-01 14:47:15 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install zip && sudo apt-get install sshpass
+# sudo apt-get install git
+# git config --global user.name "iamharshdev"
+# git config --global user.email "nitingoswami473@gmail.com"
---
+++ setup_vs.sh
@@ -0,0 +1,16 @@
+mkdir ~/code-server && cd ~/code-server
+wget https://github.com/cdr/code-server/releases/download/v3.5.0/code-server-3.5.0-linux-x86_64.tar.gz
+tar -xzvf code-server-3.5.0-linux-x86_64.tar.gz 
+sudo cp -r code-server-3.5.0-linux-x86_64 /usr/lib/code-server
+sudo ln -s /usr/lib/code-server/code-server /usr/bin/code-server
+sudo mkdir /var/lib/code-server
+sudo cp -r code-server.service /lib/systemd/system/
+sudo systemctl start code-server
+sudo systemctl status code-server
+sudo systemctl enable code-server
+sudo systemctl enable code-server --y
+sudo cp -r code-server.conf /etc/nginx/sites-available/
+rm -rf /etc/nginx/sites-enabled/default 
+sudo ln -s /etc/nginx/sites-available/code-server.conf /etc/nginx/sites-enabled/code-server.conf
+sudo nginx -t && sudo systemctl restart nginx
+echo "Ready to rock..."
