c0777dd37f4884efb66e81c621a9d4b20250aa55
iamharshdev <nitingoswami473@gmail.com>
2020-11-01 15:34:05 +0000

    Fix script a bit

--- setup_vs.sh
+++ setup_vs.sh
@@ -4,12 +4,12 @@ tar -xzvf code-server-3.5.0-linux-x86_64.tar.gz
 sudo cp -r code-server-3.5.0-linux-x86_64 /usr/lib/code-server
 sudo ln -s /usr/lib/code-server/code-server /usr/bin/code-server
 sudo mkdir /var/lib/code-server
-sudo cp -r code-server.service /lib/systemd/system/
+sudo cp -r ~/*/code-server.service /lib/systemd/system/
 sudo systemctl start code-server
 sudo systemctl status code-server
 sudo systemctl enable code-server
-sudo systemctl enable code-server --y
-sudo cp -r code-server.conf /etc/nginx/sites-available/
+sudo apt-get install nginx -y
+sudo cp -r ~/*/code-server.conf /etc/nginx/sites-available/
 rm -rf /etc/nginx/sites-enabled/default 
 sudo ln -s /etc/nginx/sites-available/code-server.conf /etc/nginx/sites-enabled/code-server.conf
 sudo nginx -t && sudo systemctl restart nginx
